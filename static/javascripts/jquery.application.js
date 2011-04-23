6dd2cf7740621aba1b292a77134841683dc90927
Harlan Lewis <harlan@harlanlewis.com>
2011-04-23 00:26:07 -0700

    application.js refactor
    Signed-off-by: Harlan Lewis <harlan@harlanlewis.com>

--- static/javascripts/jquery.application.js
+++ static/javascripts/jquery.application.js
@@ -1,17 +1,48 @@
+function swapContent(content) {
+    var head = "";
+    var body = content;
+    if (content.indexOf("\n~~~~\n") >= 0) {
+        content = content.split("\n~~~~\n",2);
+        head = content[0];
+        body = content[1];
+    }
+    $('.header_content').html(head);
+    $('.text_content').html(body);
+    $('a').click(function(){ window.location=$(this).attr('href'); });
+}
+
+function getInfo() {
+    var url = window.location.pathname +'/info.txt';
+    $.ajax({
+       type: "GET",
+       url: url,
+       success: swapContent
+    });
+    return;
+}
+
 function pageLandingInteractions(context){
     getInfo();
 }
 
+function editCallback(data) {
+    data = jQuery.parseJSON(data);
+    if (data.Code == 1) {
+        swapContent(data.Message);
+    }
+}
+
 jQuery(document).ready(function($) {
   var context = '#page_landing';
+
   $(context).runOnScope(function(){
     var pageLanding = new pageLandingInteractions(context);
   });
   $('.text_content, .header_content').blur(function() {
       var content = $('.text_content').html();
-      if ($('.header_content').html() != '') {
+      if ($('.header_content').html() !== "") {
           content = $('.header_content').html() + "\n~~~~\n" + content;
-      };
+      }
       //alert("Sending: " +newValue);
       $.ajax({
          type: "POST",
@@ -21,33 +52,3 @@ jQuery(document).ready(function($) {
       });
   });
 });
-
-function getInfo() {
-    url = window.location.pathname +'/info.txt';
-    $.ajax({
-       type: "GET",
-       url: url,
-       success: swapContent
-    });
-    return;
-}
-
-function swapContent(content) {
-    head = "";
-    body = content;
-    if (content.indexOf("\n~~~~\n") >= 0) {
-        content = content.split("\n~~~~\n",2);
-        head = content[0];
-        body = content[1];
-    };
-    $('.header_content').html(head);
-    $('.text_content').html(body);
-    $('a').click(function(){ window.location=$(this).attr('href'); });
-}
-
-function editCallback(data) {
-    data = jQuery.parseJSON(data);
-    if (data.Code == 1) {
-        swapContent(data.Message);
-    }
-}
